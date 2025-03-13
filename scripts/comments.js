cc3eaa3c59d5b6c64f3851afcd2c4d4138a5aa8c
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-03-12 20:17:59 -0600

    comentarios

--- scripts/comments.js
+++ scripts/comments.js
@@ -5,5 +5,37 @@ if (postId !== null) {
   console.error("postId no encontrado en la URL");
 }
 
-getPost(postId);
-getComments(postId);
+$(document).ready(function(){
+  getPost(postId);
+  $('main').on('click', '.editPost', function(){
+      if(isEdit){
+          reloadPosts();  
+      }else{
+          var postId = $(this).closest('article').attr('postID');
+          var postContent = $(this).closest('article').children('p').text();
+          $(this).closest('article').find('p').replaceWith(`<textarea id="editBox">${postContent}</textarea>`);
+          var isEdit = true;
+          $('#sendEdit').on('click', function(){
+              var newContent = $('#editBox').val();
+              if (newContent.length > 3 && newContent.length < 500) {
+                  changePost(newContent, postId);
+                  reloadPosts();
+              }
+          });
+      }
+    })
+    getComments(postId);
+    $('main').on('click', '#sendComment', function(){
+      var postContent = $('#composeBox').val();
+      console.log(postContent);
+      if(postContent.length > 3 && postContent.length < 500){
+        commentPost(postId, postContent);
+      }else{
+        Swal.fire({
+        title: 'Algo salió mal',
+        text: 'No pudimos enviar el comentario',
+        icon: 'error' 
+        })
+      }
+    });
+  })
--- scripts/profile.js
+++ scripts/profile.js
@@ -1,4 +1,23 @@
-getMyPosts();
+$(document).ready(function(){
+    getMyPosts();
+    $('main').on('click', '.editPost', function(){
+        if(isEdit){
+            reloadPosts();  
+        }else{
+            var postId = $(this).closest('article').attr('postID');
+            var postContent = $(this).closest('article').children('p').text();
+            $(this).closest('article').find('p').replaceWith(`<textarea id="editBox">${postContent}</textarea>`);
+            var isEdit = true;
+            $('#sendEdit').on('click', function(){
+                var newContent = $('#editBox').val();
+                if (newContent.length > 3 && newContent.length < 500) {
+                    changePost(newContent, postId);
+                    reloadPosts();
+                }
+            });
+        }  
+    })
+})
 
 $('main').on('click','.deletePost', function(){
 var postID = $(this).closest('article').attr('postID');
