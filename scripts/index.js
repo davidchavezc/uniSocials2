6ef76c5acda220775c425f378c8b63b0fb61c6bf
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2025-03-12 19:18:47 -0600

    post comments and shii

--- scripts/index.js
+++ scripts/index.js
@@ -10,16 +10,26 @@ $(document).ready(function() {
     });
 
     $('main').on('click', '.editPost', function(){
-        var postId = $(this).closest('article').attr('postID');
-        var postContent = $(this).closest('article').children('p').text();
-        $(this).closest('article').find('p').replaceWith(`<textarea id="editBox">${postContent}</textarea>`);
-        // $(this).after('<button id="sendEdit" class="sendPost">Enviar</button>');
-
-        $('#sendEdit').on('click', function(){
-            var newContent = $('#editBox').val();
-            if (newContent.length > 3 && newContent.length < 500) {
-                changePost(newContent, postId).done(reloadPosts());
-            }
-        });
+        if(isEdit){
+            reloadPosts();  
+        }else{
+            var postId = $(this).closest('article').attr('postID');
+            var postContent = $(this).closest('article').children('p').text();
+            $(this).closest('article').find('p').replaceWith(`<textarea id="editBox">${postContent}</textarea>`);
+            var isEdit = true;
+            $('#sendEdit').on('click', function(){
+                var newContent = $('#editBox').val();
+                if (newContent.length > 3 && newContent.length < 500) {
+                    changePost(newContent, postId);
+                    reloadPosts();
+                }
+            });
+        }  
     });
 });
+
+$('main').on('click','.deletePost', function(){
+    var postID = $(this).closest('article').attr('postID');
+    $(this).closest('article').remove() 
+    deletePost(postID)
+})
